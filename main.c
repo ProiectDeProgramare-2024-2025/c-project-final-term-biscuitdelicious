6357b7cfb0b7a94c6bc3b4de2d5c45c4a174234c
Delinschii Vlad <delinschiivlad@Delinschiis-MacBook-Pro.local>
2025-03-13 22:59:01 +0200

    Add functions for files and change clearing the terminal
    Took 1 hour 31 minutes

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 #define PAIRS 20
@@ -9,6 +10,9 @@
 #define MAX_DAYS 7
 #define MAX_SLOTS 5
 #define MAX_SPECIALTIES 5
+#define MAX_DOCTORS 50
+#define MAX_PATIENTS 100
+#define MAX_APPOINTMENTS 100
 
 // Hard code un dictionar
 struct Dictionary {
@@ -30,21 +34,33 @@ typedef struct {
     char timeSlots[MAX_SLOTS][STR_LEN];
 } Doctor;
 
+
 typedef struct {
     char name[STR_LEN];
     char surname[STR_LEN];
     char phoneNumber[STR_LEN];
 } Pacient;
 
-// Clear the terminal screen
+
+typedef struct {
+    Pacient pacient;
+    Doctor doctor;
+    int day;
+    int timeSlot;
+    char date[STR_LEN];
+} Appointment;
+
+// Clear the terminal screen, still doesn't work..
 void clearScreen() {
 #ifdef _WIN32
     system("cls");
-#endif
-#ifdef linux
+#elif __linux__
+    system("clear");
+#elif  __APPLE__
     system("clear");
 #endif
 }
+
 
 // Show Welcome Menu
 void menuOne() {
@@ -54,6 +70,7 @@ void menuOne() {
     printf("\nTe rog sa alegi din urmatoarele optiuni: \n\n");
     printf("(1) Vizualizeaza specialitatile disponibile \n(2) Iesi din aplicatie\n\n> ");
 }
+
 
 // Show Specialties Menu
 void specialtiesMenu(char specialties[][STR_LEN], int size_list) {
@@ -69,12 +86,116 @@ void specialtiesMenu(char specialties[][STR_LEN], int size_list) {
     printf("\n> ");
 }
 
+
 void removeEndLine(char *target) {
     char* newline = strchr(target, '\n');
     if(newline) {
         *newline = '\0';
     }
 }
+
+
+void saveDoctorsToFile(Doctor doctors[], int count) {
+    FILE *file = fopen("doctors.dat", "wb");
+    if(file == NULL) {
+        printf("A aparut o eroare la salvarea doctorilor in fisier!\n");
+        return;
+    }
+
+    fwrite(&count, sizeof(int), 1, file);
+    fwrite(doctors, sizeof(Doctor), count, file);
+    fclose(file);
+    sleep(1);
+    printf("Doctori salvati cu success!\n");
+}
+
+int loadDoctorsFromFile(Doctor doctors[], int maxDoctors) {
+    FILE *file = fopen("doctors.dat", "rb");
+    if(file == NULL) {
+        printf("Nu s-a gasit fisierul cu doctori\n");
+        return 0;
+    }
+
+    int count;
+    fread(&count, sizeof(int), 1, file);
+    if(count > maxDoctors) {
+        count = maxDoctors;
+    }
+    fread(doctors, sizeof(Doctor), count, file);
+    fclose(file);
+    printf("S-au incarcat %d doctori din fisier.\n", count);
+    return count;
+}
+
+void savePatientsToFile(Pacient patients[], int count) {
+    FILE *file = fopen("patients.dat", "wb");
+    if(file == NULL) {
+        printf("A aparut o eroare la salvarea pacientilor in fisier! \n");
+        return;
+    }
+
+    fwrite(&count, sizeof(int), 1, file);
+    fwrite(patients, sizeof(Pacient), count, file);
+    fclose(file);
+    sleep(1);
+    printf("Pacienti salvati cu success!\n");
+
+}
+
+int loadPatientsFromFile(Pacient patients[], int maxPatients) {
+    FILE *file = fopen("patients.dat", "rb");
+    if(file == NULL) {
+        printf("Nu s-a gasit fisierul pacienti\n");
+        return 0;
+    }
+
+    int count;
+    fread(&count, sizeof(int), 1, file);
+    if(count > maxPatients) {
+        count = maxPatients;
+    }
+
+    fread(patients, sizeof(Pacient), count, file);
+    fclose(file);
+    sleep(1);
+    printf("S-au incarcat %d pacienti din fisier \n, count);
+    return count;
+}
+
+void saveAppointmentsToFile(Appointment appointments[], int count) {
+    FILE *file = fopen("appointments.dat", "wb");
+
+    if(file == NULL) {
+        printf("Nu s-a gasit fisierul cu programari\n");
+        return;
+    }
+
+    fwrite(&count, sizeof(int), 1, file);
+    fwrite(appointments, sizeof(Appointment), count, file);
+    fclose(file);
+    sleep(1);
+    printf("Appointment salvati cu success!\n");
+}
+
+int loadAppointmentsFromFile(Appointment appointments[], int maxAppointments) {
+    FILE *file = fopen("appointments.dat", "rb");
+    if(file == NULL) {
+        printf("Nu s-a gasit fisierul cu programari. \n");
+        return 0;
+    }
+
+    int count;
+    fread(&count, sizeof(int), 1, file);
+
+    if(count > maxAppointments) {
+        count = maxAppointments;
+    }
+    fread(appointments, sizeof(Appointment), count, file);
+    fclose(file);
+    printf("S-au incarcat %d programari din fisier.\n", count);
+    return count;
+}
+
 
 
 
@@ -95,7 +216,6 @@ int main() {
         "Psihiatrie",
         "Urologie"
     };
-    int sizeListaSpecialitati = sizeof(listaSpecialitati) / sizeof(listaSpecialitati[0]);
 
     Doctor doctors[] = {
         {
@@ -181,8 +301,14 @@ int main() {
     };
 
     Pacient pacienti = {};
-
+    Appointment appointment[MAX_APPOINTMENTS];
+    int sizeAppointment = sizeof(appointment) / sizeof(appointment[0]);
     int doctorsCount = sizeof(doctors) / sizeof(doctors[0]);
+    int sizeListaSpecialitati = sizeof(listaSpecialitati) / sizeof(listaSpecialitati[0]);
+    patientCount = loadPatients(appointment, MAX_PATIENTS);
+    appointmentCount = loadAppointmentsFromFile(appointment, MAX_APPOINTMENTS);
+    saveAppointmentsToFile(doctors, doctorsCount);
+
 
     int userOption;
     char userChoice;
@@ -253,7 +379,9 @@ int main() {
                     removeEndLine(pacienti.phoneNumber);
                     printf("\n\n");
                     sleep(2);
-                    printf("%s %s, %s", pacienti.name, pacienti.surname, pacienti.phoneNumber);
+
+                    printf("\n Te rog sa alegi si o zi in care ai fi disponibil: ");
+                    fgets(appointment->day,)
                 }
 
             }
@@ -276,7 +404,6 @@ int main() {
 
     return 0;
 }
-
 /*
 *Utilizatorul aplicației poate vizualiza medicii și specialitățile disponibile și poate programa o consultație. DONE
 Aplicația permite adăugarea/ștergerea unei programări pentru un anumit pacient cu nume, căutarea unui medic după nume, specialitate și program, programarea consultației și anularea consultației.
